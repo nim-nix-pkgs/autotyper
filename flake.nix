e28ee29a3d4dc8c563172486ee9a477d4ae42a81
hugosenari <hugosenari@gmail.com>
2022-05-09 20:35:20 -0300

    chore: re index autotyper

--- flake.nix
+++ flake.nix
@@ -7,22 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."autotyper-master".dir   = "master";
-  inputs."autotyper-master".owner = "nim-nix-pkgs";
-  inputs."autotyper-master".ref   = "master";
-  inputs."autotyper-master".repo  = "autotyper";
-  inputs."autotyper-master".type  = "github";
-  inputs."autotyper-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."autotyper-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."autotyper-v0_2_0".dir   = "v0_2_0";
-  inputs."autotyper-v0_2_0".owner = "nim-nix-pkgs";
-  inputs."autotyper-v0_2_0".ref   = "master";
-  inputs."autotyper-v0_2_0".repo  = "autotyper";
-  inputs."autotyper-v0_2_0".type  = "github";
-  inputs."autotyper-v0_2_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."autotyper-v0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
